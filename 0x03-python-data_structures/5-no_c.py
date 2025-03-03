439dab0bb6cd4c5c2bf6cc4c76be8de336477cc6
Mohamed Saad <mosaad05019@gmail.com>
2025-03-03 14:24:51 +0200

    pycodestyle_fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    res =  ""
+    res = ""
     for i in range(len(my_string)):
         if (my_string[i] != "c" and my_string[i] != "C"):
             res += my_string[i]
